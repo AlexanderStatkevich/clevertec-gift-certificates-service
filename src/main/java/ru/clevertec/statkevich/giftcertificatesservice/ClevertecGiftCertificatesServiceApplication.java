134e2f2cf0027095894f19de266fe744be5e7abe
AlexanderStatkevich <aastatkevich@gmail.com>
2023-05-11 21:43:26 +0300

    project transition to spring boot implemented

---
+++ src/main/java/ru/clevertec/statkevich/giftcertificatesservice/ClevertecGiftCertificatesServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.statkevich.giftcertificatesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClevertecGiftCertificatesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClevertecGiftCertificatesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/clevertec/statkevich/giftcertificatesservice/ClevertecGiftCertificatesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.statkevich.giftcertificatesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClevertecGiftCertificatesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
